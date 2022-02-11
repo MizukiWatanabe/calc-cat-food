7c9dfe3f76ede98a108c9fed6d48e0eb2b723699
mw <ni4nte@gmail.com>
2022-02-11 23:44:01 +0900

    [add] favicon追加

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import 'tailwindcss/tailwind.css';
 import App from './App';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 // import './App.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HelmetProvider>
+    <React.StrictMode>
+      <Helmet>
+        <title>猫のご飯残量計算</title>
+        <link
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>😺</text></svg>"
+          rel="icon"
+        />
+        <link
+          href="https://twemoji.maxcdn.com/v/13.0.2/72x72/1f63a.png"
+          rel="icon alternate"
+          type="image/png"
+        />
+      </Helmet>
+      <App />
+    </React.StrictMode>
+  </HelmetProvider>,
   document.getElementById('root')
 );
