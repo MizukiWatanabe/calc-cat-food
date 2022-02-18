5e103fad5fe3a47ec2d85f3d4ec04c618aa451a1
mw <ni4nte@gmail.com>
2022-02-18 22:30:41 +0900

    [add] metaタグ調整

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,10 @@ ReactDOM.render(
     <React.StrictMode>
       <Helmet>
         <title>猫のご飯残量計算</title>
+        <link
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>😺</text></svg>"
+          rel="icon"
+        />
       </Helmet>
       <App />
     </React.StrictMode>
